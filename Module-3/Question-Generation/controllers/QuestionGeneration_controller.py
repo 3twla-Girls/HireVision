7987355489c67422f02b9a75d386dba1472f534c
HabibaYossre <queenrody24@gmail.com>
2025-11-23 22:48:48 +0200

    add generate questions function and its router

---
+++ Module-3/Question-Generation/controllers/QuestionGeneration_controller.py
@@ -0,0 +1,62 @@
+from groq import Groq
+from fastapi import APIRouter
+from pydantic import BaseModel
+from dotenv import load_dotenv
+import os
+
+load_dotenv()  
+client = Groq(api_key=os.getenv("GROQ_API_KEY"))
+
+
+def generate_questions_service(job_title, skills, experience_level, num_questions):
+    
+    # 1. Load prompt file
+    with open("prompts/QuestionGeneration_template.txt", "r", encoding="utf-8") as f:
+        prompt_template = f.read()
+    
+    # 2. Fill prompt with dynamic data
+    final_prompt = prompt_template.format(
+        job_title=job_title,
+        skills=", ".join(skills),
+        experience_level=experience_level,
+        num_questions=num_questions
+    )
+
+    # 3. Call Groq LLaMA model
+    completion = client.chat.completions.create(
+        model="llama-3.3-70b-versatile",
+        messages=[
+            {
+                "role": "user",
+                "content": final_prompt
+            }
+        ],
+        temperature=0
+    )
+
+    # 4. Return model output
+    return completion.choices[0].message.content
+
+
+
+#from services.question_service import generate_questions_service
+
+router = APIRouter()
+
+class JobInfo(BaseModel):
+    job_title: str
+    skills: list
+    experience_level: str
+    num_questions: int
+
+@router.post("/generate-questions")
+async def generate_questions(job_info: JobInfo):
+
+    result = generate_questions_service(
+        job_title=job_info.job_title,
+        skills=job_info.skills,
+        experience_level=job_info.experience_level,
+        num_questions=job_info.num_questions
+    )
+
+    return {"questions": result}
